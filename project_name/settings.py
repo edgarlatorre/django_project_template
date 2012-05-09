569d31f4bfcf2495c96b050b311e04fb5b5fc0b5
Edgar <edgar.romero@digitalcube.com.br>
2012-05-09 17:48:35 -0300

    Adicionando arquivo requirements e nose no settings
    Adicionei requirements com algumas bibliotecas iniciais
    Configurado django-nose no settings

--- project_name/settings.py
+++ project_name/settings.py
@@ -20,6 +20,11 @@ DATABASES = {
     }
 }
 
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = [
+   '--verbosity=2'
+]
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
